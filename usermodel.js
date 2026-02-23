383fac803eebafc061d420806a4539cdc3267f50
Piyush Goyal <piyush121goyal@gmail.com>
2026-02-23 06:14:09 +0530

    io7yky

--- usermodel.js
+++ usermodel.js
@@ -12,4 +12,4 @@ const userSchema=mongoose.Schema({
 module.exports = mongoose.model("user", userSchema)
 //model is created and needed for CRUD operations, without creating model we cant do crud operations
 
-//not there 
+//uby
